dd3468f393132b68908a7294acc6bf049ee01478
Kairat <kairataliakbarov@mail.ru>
2019-01-02 17:18:44 +0600

    addNameProfile

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
-function Result(){
-    var login = document.getElementById("login").value;
-    var password = document.getElementById("password").value;
+var LOGIN_NAME = "loginName";
 
-    if(login == "Kairat" && password == "garnier12"){
-        document.write("Добро пожаловать " + login);
-    }else{
-        alert("Неверный логин или пароль")
-        
-    }
+function result() {
+  var login = document.getElementById("login").value;
+  var password = document.getElementById("password").value;
+
+  if (login == "Kairat" && password == "garnier12") {
+    localStorage.setItem(LOGIN_NAME, login);
+    alert("Добро пожаловать " + login);
+    window.location = 'EnterNewLocation/javaHTML.html';
+  } else {
+    alert("Неверный логин или пароль");
+  }
 }
+
+function loadData() {
+  var textSpan = document.getElementById("textspan");
+  var login = localStorage.getItem(LOGIN_NAME);
+  textSpan.innerText = login;
+}
+
