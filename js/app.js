3ee7d490638f9ed8141c36c9f2b39d6022897726
Kairat <kairataliakbarov@mail.ru>
2019-01-23 16:21:10 +0600

    regist

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ window.onload = function () {
   }
 }
 
-function buttonClick() {
+function registrClick() {
   console.log("work");
   document.getElementById("modalWin").style.display = "block";
   sessionStorage.setItem('display', 'block');
@@ -22,4 +22,44 @@ window.onclick = function(event) {
     document.getElementById('modalWin').style.display = 'none';
     sessionStorage.setItem('display', 'none');
   }
+}
+
+function testLogin(login){
+  if(/^[a-zA-z]{1}[a-zA-Z1-9]{3,20}$/.test(login) === false) {
+    alert('Вы неверно ввели логин. Пример: kairat'); 
+    return false;
+  }
+  return true;
+}
+
+function testPassword(password){
+  if(/^[a-zA-z]{1}[a-zA-Z1-9]{3,20}$/.test(password) === false) {
+    alert('Вы неверно ввели пороль. Пример: aqazwsx12'); 
+    return false;
+  }
+  return true;
+}
+
+function testEmail(email) {
+  debugger;
+  for(var i = 0; i < email.length; i++) {
+    if(email[i] == '@'){
+      return true;
+    }
+  }
+  alert('Email должен содержать символ - @');
+  return false;
+}
+
+function registrModalClick() {
+  var loginWin = document.getElementById("loginWin").value;
+  var resultLogin = testLogin(loginWin);
+  var passwordWin = document.getElementById("passwordWin").value;
+  var resultPassword = testPassword(passwordWin);
+  var emailWin = document.getElementById("emailWin").value;
+  var resultEmail = testEmail(emailWin);
+  if(resultLogin == true && resultPassword == true && resultEmail == true) {
+    localStorage.setItem('login', loginWin);
+    localStorage.setItem('password', passwordWin);
+  } 
 }
